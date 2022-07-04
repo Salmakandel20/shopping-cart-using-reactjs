3cbd65a779976a5a92bf562b086aba7d75c16bf4
unknown <skandeel434@gmail.com>
2022-07-05 01:51:23 +0200

    footer commit

--- client/src/componets/Header/Header.js
+++ client/src/componets/Header/Header.js
@@ -1,9 +1,10 @@
 // import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { words } from '../../words';
 import "../../css/Header/Header.css";
  function Header() {
   return (
-    <header>Shopping cart</header>
+    <header>{words.headertitle}</header>
   )
 }
 export default Header
--- client/src/componets/footer/Footer.js
+++ client/src/componets/footer/Footer.js
@@ -1,10 +1,11 @@
 // import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { words } from '../../words';
 import "../../css/footer/footer.css";
 
  function Footer() {
   return (
-    <footer>&copy; CopyWright Hamza Nabil @ 2022</footer>
+    <footer>{words.footertitle}</footer>
   )
 }
 export default Footer
